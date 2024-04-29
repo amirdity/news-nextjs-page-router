24b53ea2d2fd40d8b17b3a32b9bc761c28de53c8
amir fattahi <amirfattahi22@gmail.com>
2024-04-29 22:43:28 +0330

    +axios

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
-import Image from "next/image";
-import Login from "./components/axios-auth/Login";
-// import Login from "./components/firebase/LogIn";
-// import SignUp from "./components/firebase/SignUp";
-// import LoginSignup from "./components/LogIn/LoginSignup";
+
+import Title from "./components/Title";
+import Axios from "./components/Axios";
+import Train from './components/Train'
 export default function Home() {
   return (
-    <main>
-      <div>
-        {/* <Login/>
-        <SignUp/>
-        <LoginSignup/> */}
-        <Login/>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        {/* <Title/> */}
+        {/* <Axios/> */}
+        <Train/>
+
+
+
       </div>
     </main>
   );
