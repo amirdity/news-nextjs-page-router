3f231b086a1c8b419344f0dfa41265cee2348e07
amir fattahi <amirfattahi22@gmail.com>
2024-05-01 17:32:39 +0330

    Add files via upload

--- app/components/form/InputProp.jsx
+++ app/components/form/InputProp.jsx
@@ -5,11 +5,11 @@ function InputProp(props) {
   return (
     <div>
       <div className="flex flex-row">
-        <label className="m-5 w-[150px] flex mx-10">{props.name}</label>
-        <input name={props.name} type={props.type} placeholder={props.placeholder} className="border-blue-600 border-solid border my-2 p-2"/>
+        <label className="m-5 w-[150px] flex mx-10 font-sans">{props.label}</label>
+        <input name={props.name} type={props.type} placeholder={props.placeholder} className="my-2 p-2" />
       </div>
     </div>
   );
 }
-
+ 
 export default InputProp;
--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Input from "./components/form/InputProp";
 import RegisterForm from "./components/form/RegisterForm";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-sans">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         {/* <Title/> */}
         {/* <Axios/> */}
